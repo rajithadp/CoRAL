d8ff4eed450724546522b268ed7a7a0324513129
Suhas Rao <surao@ucsd.edu>
2025-01-10 03:56:56 -0800

    pending one last triage

--- coral/cli.py
+++ coral/cli.py
@@ -8,7 +8,14 @@ from typing import Annotated
 import pysam
 import typer
 
-from coral import cycle2bed, cycle_decomposition, hsr, plot_amplicons, plot_cn
+from coral import (
+    bam_types,
+    cycle2bed,
+    cycle_decomposition,
+    hsr,
+    plot_amplicons,
+    plot_cn,
+)
 from coral.breakpoint import infer_breakpoint_graph
 from coral.cnv_seed import run_seeding
 from coral.datatypes import Solver
@@ -192,6 +199,7 @@ def cycle_decomposition_mode(
 ) -> None:
     bb = infer_breakpoint_graph.LongReadBamToBreakpointMetadata(
         lr_bamfh=pysam.AlignmentFile(str(lr_bam), "rb"),
+        bam=bam_types.BAMWrapper(str(lr_bam), "rb"),
         lr_graph=[pickle.load(bp_graph)],
     )  # type: ignore[arg-type]
     # bb.fetch_breakpoint_reads()
